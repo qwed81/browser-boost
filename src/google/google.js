a16d5b270b6c82707295a04469ddad9f041a61d7
Josh Cox <jcox.12.2002@gmail.com>
2022-12-22 17:10:54 -0600

    changed to highlighting links

--- src/google/google.js
+++ src/google/google.js
@@ -1,27 +1,77 @@
 let elements = getSearchElements();
+console.log(elements);
+
 let highlighted;
 highlight(0);
 window.scrollTo({
 	top: 0
 });
 
+let jumpList = {
+	'u': 0,
+	'i': 1,
+	'o': 2,
+	'p': 3,
+	'8': 4,
+	'9': 5,
+	'0': 6,
+};
+
+document.body.addEventListener('keydown', (e) => {
+	let key = e.key;
+	// skip modified values
+	if (key.ctrlKey == true || key.altKey == true) {
+		return;
+	}
+
+	if (key == ' ') {
+		e.preventDefault();
+		applyLink(elements[highlighted]);
+	} else if (key == 'j') {
+		e.preventDefault();
+		highlightNext();
+	} else if (key == 'k') {
+		e.preventDefault();
+		highlightPrev();
+	} else if (jumpList[key]) {
+		e.preventDefault();
+		highlightAndScroll(jumpList[key]);
+	} 
+
+});
+
+// "generic" over lists and classList
+function disjoint(classList1, classList2) {
+	for (let className1 of classList1) {
+		for (let className2 of classList2) {
+			if (className1 == className2) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
 function getSearchElements() {
-	let validClasses = ['V9qXjb', 'MjjYud', 'hlcw0c'];
+	let validParentClasses = ['V9qXjb', 'MjjYud', 'hlcw0c'];
 	let topParent = document.getElementById('rso');
+	
 	let elems = [];
 	for (let child of topParent.childNodes) {
-		for (let className of validClasses) {
-			if (child.classList.contains(className)) {
-				elems.push(child);
-				break;
-			}
+		// it is not a valid class
+		if (disjoint(child.classList, validParentClasses)) {
+			continue;
+		}
+
+		let linkElements = child.querySelectorAll('a');
+		for (let i = 0; i < linkElements.length; i++) {
+			elems.push(linkElements[i]);
 		}
 	}
 	
 	return elems;
 }
-
-const selected = 'boost-selected';
 
 function highlight(index) {
 	for (elem of elements) {
@@ -29,7 +79,7 @@ function highlight(index) {
 	}
 
 	let element = elements[index];
- 	element.classList.add(selected);
+ 	element.classList.add('boost-selected');
 	highlighted = index;
 }
 
@@ -67,46 +117,7 @@ function highlightPrev() {
 	}
 }
 
-function applyLink(element) {
-	const link = element.querySelector('a');
+function applyLink(link) {
 	window.location.href = link.href;
 }
 
-document.body.addEventListener('keydown', (e) => {
-	let key = e.key;
-//	console.log(key);
-
-	// skip modified values
-	if (key.ctrlKey == true || key.altKey == true) {
-		return;
-	}
-
-	if (key == 'j') {
-		e.preventDefault();
-		highlightNext();
-	}
-	else if (key == 'k') {
-		e.preventDefault();
-		highlightPrev();
-	}
-	else if (key == 'u') {
-		e.preventDefault();
-		highlightAndScroll(0);
-	}
-	else if (key == 'i') {
-		e.preventDefault();
-		highlightAndScroll(1);
-	}
-	else if (key == 'o') {
-		e.preventDefault();
-		highlightAndScroll(2);
-	}
-	else if (key == 'p') {
-		e.preventDefault();
-		highlightAndScroll(3);
-	}
-	else if (key == ' ') {
-		e.preventDefault();
-		applyLink(elements[highlighted]);
-	}
-});
